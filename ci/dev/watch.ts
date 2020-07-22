247ceb874b13ad19fbeb50ce69f429a773b150c6
Asher <ash@coder.com>
2020-07-22 15:28:33 -0500

    Remove unused endpoints
    - dashboard
    - app api

--- ci/dev/watch.ts
+++ ci/dev/watch.ts
@@ -145,7 +145,6 @@ class Watcher {
   private createBundler(out = "dist"): Bundler {
     return new Bundler(
       [
-        path.join(this.rootPath, "src/browser/pages/app.ts"),
         path.join(this.rootPath, "src/browser/register.ts"),
         path.join(this.rootPath, "src/browser/serviceWorker.ts"),
       ],
